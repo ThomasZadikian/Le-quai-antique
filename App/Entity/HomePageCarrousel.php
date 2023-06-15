46791f0f3ddea49087f9d15e1287a26ce6d0503c
Thomas <Thoma.zadikian@gmail.com>
2023-06-15 08:15:57 +0200

    Fix bug with caroussel when more than 3 images was push into it

--- App/Entity/HomePageCarrousel.php
+++ App/Entity/HomePageCarrousel.php
@@ -18,36 +18,42 @@ class HomePageCarrousel
         $carrouselImageLabels = $this->config['carrouselImageLabels'];
         $carrouselImageDescriptions = $this->config['carrouselImageDescriptions'];
 
+        $totalImages = count($carrouselImages); // Nombre total d'images
+
         echo '<div id="carouselExampleCaptions" class="carousel slide">
-            <div class="carousel-indicators">';
-        foreach ($carrouselImages as $index => $image) {
-            echo '<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="' . $index . '"';
-            echo ($index === 0) ? ' class="active" aria-current="true"' : '';
-            echo ' aria-label="Slide ' . ($index + 1) . '"></button>';
+        <div class="carousel-indicators">';
+        for ($i = 0; $i < $totalImages; $i++) {
+            echo '<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="' . $i . '"';
+            echo ($i === 0) ? ' class="active" aria-current="true"' : '';
+            echo ' aria-label="Slide ' . ($i + 1) . '"></button>';
         }
         echo '</div>
-            <div class="carousel-inner">';
+        <div class="carousel-inner">';
         foreach ($carrouselImages as $index => $image) {
             echo '<div class="carousel-item';
             echo ($index === 0) ? ' active' : '';
             echo '">
-                <img src="' . $uploadsDirectory . $image . '" class="d-block presentationImg" alt="...">
-                <div class="carousel-caption d-none d-md-block caption-bg">
-                    <h5>' . $carrouselImageLabels[$index] . '</h5>
-                    <p>' . $carrouselImageDescriptions[$index] . '</p>
-                </div>
+                <img src="' . $uploadsDirectory . $image . '" class="d-block presentationImg" alt="' . $image . '">
+                <div class="carousel-caption d-none d-md-block caption-bg">';
+            if (isset($carrouselImageLabels[$index])) {
+                echo '<h5>' . $carrouselImageLabels[$index] . '</h5>';
+            }
+            if (isset($carrouselImageDescriptions[$index])) {
+                echo '<p>' . $carrouselImageDescriptions[$index] . '</p>';
+            }
+            echo '</div>
             </div>';
         }
         echo '</div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div>
-    </div>';
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div>';
     }
 }
