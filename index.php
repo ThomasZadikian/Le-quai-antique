ec850a668107ea95dfebaf1f2e5c6fcca53d0b0f
Thomas <Thoma.zadikian@gmail.com>
2023-06-19 12:35:58 +0200

    set header

--- index.php
+++ index.php
@@ -5,5 +5,9 @@ spl_autoload_register();
 
 use App\Controller\Controller;
 
+header("Content-Security-Policy: default-src 'self'");
+header("X-Frame-Options: DENY");
+header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
+
 $controller = new Controller();
 $controller->route();
