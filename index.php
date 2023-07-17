0e79bdb4818f3c3f990f0489a53700fd23bf529c
Thomas <Thoma.zadikian@gmail.com>
2023-07-17 07:16:10 +0200

    Bug fixe

--- index.php
+++ index.php
@@ -5,9 +5,5 @@ spl_autoload_register();
 
 use App\Controller\Controller;
 
-header("Content-Security-Policy: default-src 'self'");
-header("X-Frame-Options: DENY");
-header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
-
 $controller = new Controller();
 $controller->route();
